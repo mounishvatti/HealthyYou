7e4278e575d82255fc9ae29cf6a6507f90dfc38e
Anurag paul <114428908+Anurag30032004@users.noreply.github.com>
2023-09-25 19:25:52 +0530

    Update signin.js

--- src/assets/js/signin.js
+++ src/assets/js/signin.js
@@ -1,25 +1,8 @@
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    const upLink = document.querySelector("a.up");
-
-    upLink.addEventListener("click", function (event) {
-        event.preventDefault();
-
-        const id1Element = document.getElementById("id1");
-        id1Element.textContent = "Sign-up";
-
-        const id2Element = document.getElementById("id2");
-        id2Element.textContent = "Sign-up";
-
-        const id3Element = document.getElementById("id3");
-        id3Element.textContent = "Sign-up with Google";
-
-        const id4Element = document.getElementById("id4");
-        id4Element.textContent = "Register for a NEW Experience";
-
-        const id5Element = document.getElementById("id5");
-        id5Element.textContent = "Join us for exciting offers.";
-
-    });
+document.getElementById("id6").addEventListener("click", (event) => {
+    event.preventDefault();
+    document.getElementById("id2").innerHTML = "Sign Up";
+    document.getElementById("id1").innerHTML = "Sign Up";
+    document.getElementById("id3").innerHTML = "Sign Up with Google";
+    document.getElementById("id4").innerHTML = "Register for a NEW Experience";
+    document.getElementById("id5").innerHTML = "Join us for exciting offers.";
 });
